781eed344b952ec1c23138546686e608c58c80f8
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2023-10-08 20:21:36 -0700

    modified styles to both ios and android

--- App.js
+++ App.js
@@ -3,13 +3,17 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native'
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState('')
+  const [courseGoals, setCourseGoals] = useState([])
 
   function goalInputHandler(enteredText) {
     setEnteredGoalText(enteredText)
   }
 
   function addGoalHandler() {
-    console.log(enteredGoalText)
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      enteredGoalText,
+    ])
   }
 
   return (
@@ -23,7 +27,11 @@ export default function App() {
         <Button title="ADD Goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        <Text>List of goals...</Text>
+        {courseGoals.map((goal) => (
+          <View key={goal} style={styles.goalItem}>
+            <Text style={styles.goalText}>{goal}</Text>
+          </View>
+        ))}
       </View>
     </View>
   )
@@ -53,5 +61,14 @@ const styles = StyleSheet.create({
   },
   goalsContainer: {
     flex: 5,
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: '#6499E9',
+  },
+  goalText: {
+    color: '#ffffff',
   },
 })
