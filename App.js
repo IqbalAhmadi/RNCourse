43ef1620a26bb3fb4d3b32290f291bd07aa22040
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2023-10-08 19:47:40 -0700

    created handler functions to properly render them

--- App.js
+++ App.js
@@ -1,23 +1,57 @@
-import { StyleSheet, Text, View, Button } from 'react-native'
+import { useState } from 'react'
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native'
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState('')
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText)
+  }
+
+  function addGoalHandler() {
+    console.log(enteredGoalText)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text
-        style={{ margin: 16, borderWidth: 2, borderColor: 'red', padding: 16 }}
-      >
-        Demo App
-      </Text>
-      <Button title="Click Me" onPress={() => alert('Hello World')} />
+    <View style={styles.appContaiber}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your goal!"
+          onChangeText={goalInputHandler}
+        />
+        <Button title="ADD Goal" onPress={addGoalHandler} />
+      </View>
+      <View style={styles.goalsContainer}>
+        <Text>List of goals...</Text>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContaiber: {
     flex: 1,
-    backgroundColor: '#fff',
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccccc',
+  },
+  textInput: {
+    borderColor: '#cccccccc',
+    borderWidth: 1,
+    padding: 8,
+    marginRight: 8,
+    width: '70%',
+  },
+  goalsContainer: {
+    flex: 5,
   },
 })
